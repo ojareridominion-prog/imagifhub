c99e44e188e4ecaf01c4b60c8ce62a2118e5bec5
ojareridominion-prog <ojareridominion@gmail.com>
2025-12-28 22:47:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ IMGBB_API_KEY = os.environ["IMGBB_API_KEY"]
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_SERVICE_ROLE_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
 app = FastAPI()
