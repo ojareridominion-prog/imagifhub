9172e7aa05af9c75f852550ac8c16b18d6b0f2ed
ojareridominion-prog <ojareridominion@gmail.com>
2025-12-31 20:44:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ let currentCategory = "Featured";
 const musicLibrary = {
     "Featured": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"],
     "Nature": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"],
+    "Places": [],
+    "Aesthetic": [],
+    "Cars": [],
+    "Luxury": [],
+    "Anime": [],
+    "Animals": [],
+    "Ancient": [],
     "Default": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"]
 };
 
