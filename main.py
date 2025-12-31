6c644efadcec55654dd3060ab282793819c92ee3
ojareridominion-prog <ojareridominion@gmail.com>
2025-12-31 18:58:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class AdminUpload(StatesGroup):
 # Must match your script.js categories exactly
 CATEGORIES = [
     "Nature", "Places", "Aesthetic", "Cars", 
-    "Luxury", "Anime", "Animals", "Ancient", "marine", "Others"
+    "Luxury", "Anime", "Animals", "Ancient", "Marine", "Art", "Others"
 ]
 
 # ==================== WEBHOOK HELPERS ====================
