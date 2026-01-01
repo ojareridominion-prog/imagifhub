d99421378069a3b1f83690483599baa4bf7a1238
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 14:36:34 +0100

    Update music.js

--- music.js
+++ music.js
@@ -8,11 +8,13 @@ export const musicLibrary = {
         "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=stylish-rock-446683.mp3",
         "https://cdn.pixabay.com/download/audio/2025/10/14/audio_d7b510058d.mp3?filename=rock-420049.mp3",
         "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=energetic-rock-333526.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/03/18/audio_c38aba1e54.mp3?filename=upbeat-motivation-rock-315219.mp3"
                 ],
     "Nature": [
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=smilewave-pop-summer-443398.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=smilewave-pop-summer-443398.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/03/18/audio_c38aba1e54.mp3?filename=upbeat-motivation-rock-315219.mp3"
     ],
     "Places": [
         "https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3", 
@@ -27,7 +29,8 @@ export const musicLibrary = {
     "Aesthetic": [
         "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=stylish-rock-446683.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=stylish-rock-446683.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/03/18/audio_c38aba1e54.mp3?filename=upbeat-motivation-rock-315219.mp3"
     ],
     "Cars": [
         "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3",
@@ -40,7 +43,8 @@ export const musicLibrary = {
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=smilewave-pop-summer-443398.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=stylish-rock-446683.mp3",
         "https://cdn.pixabay.com/download/audio/2025/10/14/audio_d7b510058d.mp3?filename=rock-420049.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=energetic-rock-333526.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=energetic-rock-333526.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/03/18/audio_c38aba1e54.mp3?filename=upbeat-motivation-rock-315219.mp3"
     ],
     "Anime": ["https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3", 
               "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
@@ -50,7 +54,8 @@ export const musicLibrary = {
     "Animals": [
                     "https://cdn.pixabay.com/download/audio/2025/10/14/audio_d7b510058d.mp3?filename=rock-420049.mp3",
                     "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=energetic-rock-333526.mp3",
-                    "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3"
+                    "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3",
+                    "https://cdn.pixabay.com/download/audio/2025/03/18/audio_c38aba1e54.mp3?filename=upbeat-motivation-rock-315219.mp3"
     ],
     "Ancient": [
                     "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3",
@@ -63,7 +68,8 @@ export const musicLibrary = {
                "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=stylish-rock-446683.mp3",
                "https://cdn.pixabay.com/download/audio/2025/10/14/audio_d7b510058d.mp3?filename=rock-420049.mp3",
                "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=energetic-rock-333526.mp3",
-               "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3"
+               "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3",
+               "https://cdn.pixabay.com/download/audio/2025/03/18/audio_c38aba1e54.mp3?filename=upbeat-motivation-rock-315219.mp3"
     ]
 };
 
