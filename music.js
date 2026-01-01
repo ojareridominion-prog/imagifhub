5fd19adda83f34b86846787cb27b5b4a361c02ee
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 08:56:10 +0100

    Create music.js

---
+++ music.js
@@ -0,0 +1,14 @@
+export const musicLibrary = {
+    "Discover": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"],
+    "Nature": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"],
+    "Places": [],
+    "Aesthetic": [],
+    "Cars": [],
+    "Luxury": [],
+    "Anime": [],
+    "Animals": [],
+    "Ancient": [],
+    "Default": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"]
+};
+
+export const categories = ["Discover", "Nature", "Places", "Aesthetic", "Cars", "Luxury", "Anime", "Animals", "Ancient"];
