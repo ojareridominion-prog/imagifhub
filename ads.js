d5959279b535a5ce8622e8f0f719f2cf5474bfa0
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 08:56:44 +0100

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,8 @@
+export const nativeAds = [
+    {
+        title: "Check this out!",
+        image: "https://via.placeholder.com/300",
+        link: "https://example.com"
+    },
+    // Add more ads here
+];
