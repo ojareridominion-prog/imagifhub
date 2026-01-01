3aa098e93254451a368fb4694b68582bcd4c4269
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 09:38:12 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,11 +1,11 @@
 export const nativeAds = [
-    {
+    /*{
         id: "premium",
         image: "ads/premium.png",
         title: "Go Premium",
         subtitle: "Remove all ads",
-        action: () => "alert('Coming Soon!')"/*openPremium()*/
-    },
+        action: () => openPremium()
+    },*/
     {
         id: "Temu",
         image: "ads/temu.png",
