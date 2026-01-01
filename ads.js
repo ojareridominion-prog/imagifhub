1dc4d937666027e36e2ca7f16b7edc8b13ab5cd4
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 08:59:52 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,8 +1,64 @@
 export const nativeAds = [
     {
-        title: "Check this out!",
-        image: "https://via.placeholder.com/300",
-        link: "https://example.com"
+        id: "premium",
+        image: "ads/premium.png",
+        title: "Go Premium",
+        subtitle: "Remove all ads",
+        action: () => openPremium()
+    },
+    {
+        id: "Temu",
+        image: "ads/temu.png",
+        title: "Sponsored",
+        subtitle: "legitimate online marketplace",
+        action: () => {
+            window.open("https://temu.to/k/eob8orcu3n0", "_blank") // Add your clickable link here
+        }
+    },
+    {
+        id: "Stake.com",
+        image: "ads/stake.png",
+        title: "Sponsored",
+        subtitle: "The Global Giant of Crypto Gambling",
+        action: () => {
+            window.open("https://stake.com/?c=hcYKazFc", "_blank") // Add your clickable link here
+        }
+    },
+    {
+        id: "bitoshi africa",
+        image: "ads/bitoshi.png",
+        title: "Sponsored",
+        subtitle: "swap, buy, sell, send and receive crypto with bitoshi africa",
+        action: () => {
+            window.open("https://bitoshi.africa/ref?username=ojareri", "_blank") // Add your clickable link here
+        }
+    },
+    {
+        id: "gemgala",
+        image: "ads/gemgala.png",
+        title: "Sponsored",
+        subtitle: "play games, chat and earn",
+        action: () => {
+            window.open("https://getblock.me/u/24239713", "_blank") // Add your clickable link here
+        }
+    },
+    {
+        id: "palmpay",
+        image: "ads/palmpay.png",
+        title: "Sponsored",
+        subtitle: "enjoy free & safe transactions and cashbacks",
+        action: () => {
+            window.open("https://info.palmpay.com/tNYOx0uv", "_blank") // Add your clickable link here
+        }
+    },
+    {
+        id: "opay",
+        image: "ads/opay.png",
+        title: "Sponsored",
+        subtitle: "enjoy free bank transfers, cashbacks and more",
+        action: () => {
+            window.open("https://opay.ng/s/2Tqtv", "_blank") // Add your clickable link here
+        }
     },
     // Add more ads here
 ];
