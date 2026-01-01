af7c4662de02e928187a79104a0ae8b25535aee4
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 09:21:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,6 @@ const API_URL = "https://imagifhub.vercel.app";
 let activeSwiper = null;
 let currentCategory = "Discover";
 
-/*const musicLibrary = {
-    "Discover": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"],
-    "Nature": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"],
-    "Places": [],
-    "Aesthetic": [],
-    "Cars": [],
-    "Luxury": [],
-    "Anime": [],
-    "Animals": [],
-    "Ancient": [],
-    "Default": ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"]
-};
-
-const categories = ["Discover", "Nature", "Places", "Aesthetic", "Cars", "Luxury", "Anime", "Animals", "Ancient"];*/
-
 const SEEN_LIMIT = 20;
 const SEEN_KEY = "imagifhub-seen-history";
 
@@ -194,72 +179,6 @@ async function shareBot() {
         console.log('Error sharing:', err);
     }
 }
-
-// ===== Native Ads Inventory =====
-/*const NATIVE_ADS = [
-    {
-        id: "premium",
-        image: "ads/premium.png",
-        title: "Go Premium",
-        subtitle: "Remove all ads",
-        action: () => openPremium()
-    },
-    {
-        id: "Temu",
-        image: "ads/temu.png",
-        title: "Sponsored",
-        subtitle: "legitimate online marketplace",
-        action: () => {
-            window.open("https://temu.to/k/eob8orcu3n0", "_blank") // Add your clickable link here
-        }
-    },
-    {
-        id: "Stake.com",
-        image: "ads/stake.png",
-        title: "Sponsored",
-        subtitle: "The Global Giant of Crypto Gambling",
-        action: () => {
-            window.open("https://stake.com/?c=hcYKazFc", "_blank") // Add your clickable link here
-        }
-    },
-    {
-        id: "bitoshi africa",
-        image: "ads/bitoshi.png",
-        title: "Sponsored",
-        subtitle: "swap, buy, sell, send and receive crypto with bitoshi africa",
-        action: () => {
-            window.open("https://bitoshi.africa/ref?username=ojareri", "_blank") // Add your clickable link here
-        }
-    },
-    {
-        id: "gemgala",
-        image: "ads/gemgala.png",
-        title: "Sponsored",
-        subtitle: "play games, chat and earn",
-        action: () => {
-            window.open("https://getblock.me/u/24239713", "_blank") // Add your clickable link here
-        }
-    },
-    {
-        id: "palmpay",
-        image: "ads/palmpay.png",
-        title: "Sponsored",
-        subtitle: "enjoy free & safe transactions and cashbacks",
-        action: () => {
-            window.open("https://info.palmpay.com/tNYOx0uv", "_blank") // Add your clickable link here
-        }
-    },
-    {
-        id: "opay",
-        image: "ads/opay.png",
-        title: "Sponsored",
-        subtitle: "enjoy free bank transfers, cashbacks and more",
-        action: () => {
-            window.open("https://opay.ng/s/2Tqtv", "_blank") // Add your clickable link here
-        }
-    },
-    
-]; */
 
 let adIndex = Number(localStorage.getItem("adIndex") || 0);
 
