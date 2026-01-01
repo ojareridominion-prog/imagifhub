106aef891f357fe5155b6178ebed1137c8f6e148
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 14:15:21 +0100

    Update music.js

--- music.js
+++ music.js
@@ -2,21 +2,27 @@ export const musicLibrary = {
     "Discover": [
         "https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3", 
         "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3"
                 ],
     "Nature": [
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
     ],
     "Places": [
         "https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3", 
-        "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3"
               ],
     "Aesthetic": [
         "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
     ],
-    "Cars": [],
-    "Luxury": [],
+    "Cars": [
+        "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3"
+    ],
+    "Luxury": [
+        "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3"
+    ],
     "Anime": ["https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3", 
               "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3"
               ],
@@ -25,6 +31,7 @@ export const musicLibrary = {
     "Default": ["https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3",
                "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
                "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
+               "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3"
     ]
 };
 
