bfc7b2aa096766d3f41fcb2051e7d77cd9367428
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-01 15:03:24 +0100

    Update music.js

--- music.js
+++ music.js
@@ -13,7 +13,8 @@ export const musicLibrary = {
         "https://cdn.pixabay.com/download/audio/2025/06/25/audio_e8f60a3cf5.mp3?filename=sad-documentary-background-music-365827.mp3",
         "https://cdn.pixabay.com/download/audio/2022/10/30/audio_fbd14dd8bc.mp3?filename=sad-moment-sad-and-melancholy-piano-background-music-124488.mp3",
         "https://cdn.pixabay.com/download/audio/2023/05/16/audio_0636f970ca.mp3?filename=sad-violin-150146.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_5276999892.mp3?filename=cvilni-violin-1-302858.mp3"
                 ],
     "Nature": [
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
@@ -35,7 +36,8 @@ export const musicLibrary = {
         "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3",
         "https://cdn.pixabay.com/download/audio/2025/06/25/audio_e8f60a3cf5.mp3?filename=sad-documentary-background-music-365827.mp3",
         "https://cdn.pixabay.com/download/audio/2025/06/13/audio_6545ba3c7c.mp3?filename=emotional-sad-guitar-music-349888.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_5276999892.mp3?filename=cvilni-violin-1-302858.mp3"
               ],
     "Aesthetic": [
         "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
@@ -46,7 +48,8 @@ export const musicLibrary = {
         "https://cdn.pixabay.com/download/audio/2025/06/13/audio_6545ba3c7c.mp3?filename=emotional-sad-guitar-music-349888.mp3",
         "https://cdn.pixabay.com/download/audio/2022/10/30/audio_fbd14dd8bc.mp3?filename=sad-moment-sad-and-melancholy-piano-background-music-124488.mp3",
         "https://cdn.pixabay.com/download/audio/2023/05/16/audio_0636f970ca.mp3?filename=sad-violin-150146.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_5276999892.mp3?filename=cvilni-violin-1-302858.mp3"
     ],
     "Cars": [
         "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3",
@@ -64,7 +67,8 @@ export const musicLibrary = {
         "https://cdn.pixabay.com/download/audio/2025/06/13/audio_6545ba3c7c.mp3?filename=emotional-sad-guitar-music-349888.mp3",
         "https://cdn.pixabay.com/download/audio/2022/10/30/audio_fbd14dd8bc.mp3?filename=sad-moment-sad-and-melancholy-piano-background-music-124488.mp3",
         "https://cdn.pixabay.com/download/audio/2023/05/16/audio_0636f970ca.mp3?filename=sad-violin-150146.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3"
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_5276999892.mp3?filename=cvilni-violin-1-302858.mp3"
     ],
     "Anime": ["https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3", 
               "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
@@ -87,7 +91,8 @@ export const musicLibrary = {
                     "https://cdn.pixabay.com/download/audio/2025/06/13/audio_6545ba3c7c.mp3?filename=emotional-sad-guitar-music-349888.mp3",
                     "https://cdn.pixabay.com/download/audio/2022/10/30/audio_fbd14dd8bc.mp3?filename=sad-moment-sad-and-melancholy-piano-background-music-124488.mp3",
                     "https://cdn.pixabay.com/download/audio/2023/05/16/audio_0636f970ca.mp3?filename=sad-violin-150146.mp3",
-                    "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3"
+                    "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3",
+                    "https://cdn.pixabay.com/download/audio/2025/02/18/audio_5276999892.mp3?filename=cvilni-violin-1-302858.mp3"
     ],
     "Default": ["https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3",
                "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
@@ -103,7 +108,8 @@ export const musicLibrary = {
                "https://cdn.pixabay.com/download/audio/2025/06/13/audio_6545ba3c7c.mp3?filename=emotional-sad-guitar-music-349888.mp3",
                "https://cdn.pixabay.com/download/audio/2022/10/30/audio_fbd14dd8bc.mp3?filename=sad-moment-sad-and-melancholy-piano-background-music-124488.mp3",
                 "https://cdn.pixabay.com/download/audio/2023/05/16/audio_0636f970ca.mp3?filename=sad-violin-150146.mp3",
-                "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3"
+                "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3",
+                "https://cdn.pixabay.com/download/audio/2025/02/18/audio_5276999892.mp3?filename=cvilni-violin-1-302858.mp3"
     ]
 };
 
