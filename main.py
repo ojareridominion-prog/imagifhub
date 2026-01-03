b0a27a6dd8ee4fbda0d5e396d58f73b9392367ff
ojareridominion-prog <ojareridominion@gmail.com>
2026-01-03 18:30:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ async def cmd_start(message: Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="🚀 Let's Go!", web_app={"url": "https://ojareridominion-prog.github.io/imagifhub/"})],
         [InlineKeyboardButton(text="⭐ Go Premium", callback_data="premium")],
-        [InlineKeyboardButton(text="📢 Official channel", web_app={"url": "https://t.me/imagifhub"})]
+        [InlineKeyboardButton(text="📢 Official channel", {"url": "https://t.me/imagifhub"})]
     ])
     await message.answer(
         "IMAGIFHUB isn't just an app—it's your personal portal to a world of endless, breathtaking beauty.\n\nDon't wait, click let's go 🚀🚀 to continue",
