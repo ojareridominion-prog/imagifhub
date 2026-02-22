62c118e9e4ca97b2919206e0080dbf24232d7f2e
ojareridominion-prog <ojareridominion@gmail.com>
2026-02-22 08:29:03 +0100

    Remove audio URLs from 'Discover' category
    Removed multiple audio URLs from the 'Discover' category in the music library.

--- music.js
+++ music.js
@@ -1,28 +1,5 @@
 export const musicLibrary = {
-    "Discover": [
-        "https://cdn.pixabay.com/download/audio/2021/11/13/audio_343081d119.mp3?filename=rammstein-style-metal-10726.mp3", 
-        "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=cloud-kicker-is-awesome-378633.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=fun-pop-background-music-419356.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=smilewave-pop-summer-443398.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=stylish-rock-446683.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/10/14/audio_d7b510058d.mp3?filename=rock-420049.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=energetic-rock-333526.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/09/23/audio_cbb893b235.mp3?filename=uplfting-rock-music-409355.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/03/18/audio_c38aba1e54.mp3?filename=upbeat-motivation-rock-315219.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/06/25/audio_e8f60a3cf5.mp3?filename=sad-documentary-background-music-365827.mp3",
-        "https://cdn.pixabay.com/download/audio/2022/10/30/audio_fbd14dd8bc.mp3?filename=sad-moment-sad-and-melancholy-piano-background-music-124488.mp3",
-        "https://cdn.pixabay.com/download/audio/2023/05/16/audio_0636f970ca.mp3?filename=sad-violin-150146.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_bbcb0e9b17.mp3?filename=cvilni-violin-302859.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/02/18/audio_5276999892.mp3?filename=cvilni-violin-1-302858.mp3",
-        "https://cdn.pixabay.com/download/audio/2024/07/26/audio_ae5558d34f.mp3?filename=bouncy-flute-and-keys-hard-trap-beat-prod-by-onesevenbeatxs-227243.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/03/18/audio_7d5c12b31a.mp3?filename=dont-talk-315229.mp3",
-        "https://cdn.pixabay.com/download/audio/2023/11/04/audio_61107b7ef6.mp3?filename=bounce-gone-crazy-174657.mp3",
-        "https://cdn.pixabay.com/download/audio/2023/09/13/audio_635b38f516.mp3?filename=vapor-club-bounce-music-166350.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/03/18/audio_3ff2b9369c.mp3?filename=promo-advertising-funk-315207.mp3",
-        "https://cdn.pixabay.com/download/audio/2023/08/09/audio_f3b6314859.mp3?filename=rap-beats-music-161432.mp3",
-        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_1c1f8db6b0.mp3?filename=rap-beat-beats-music-444108.mp3"
-                ],
+    
     "Nature": [
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=soft-piano-background-444129.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=smilewave-pop-summer-443398.mp3",
