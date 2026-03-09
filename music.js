314cda1cdcca9c98b4d5e4b790f517173de699ad
ojareridominion-prog <ojareridominion@gmail.com>
2026-03-09 01:47:20 +0100

    Update music.js

--- music.js
+++ music.js
@@ -1,54 +1,62 @@
 export const musicLibrary = {
     
     "Nature": [
-
+        
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
     ],
     
     "Places": [
-
+        
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
               ],
     
     "Aesthetic": [
-
+        
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
     ],
     
     "Cars": [
-
+        
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
     ],
     
     "Luxury": [
 
-
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
     ],
     
     "Art": [
-
+        
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
               ],
     
     "Animals": [
-
+        
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
     ],
     
     "Historical": [
 
-
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
     ],
     
       "Ai-generated": [
-
+          
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
           
     ],
     
     "Default": [
-
+        
+"https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         
     ]
     
