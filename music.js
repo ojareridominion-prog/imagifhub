153a0368aba49e46bbf316055721a22e6857a1e2
ojareridominion-prog <ojareridominion@gmail.com>
2026-03-09 02:04:11 +0100

    Update music.js

--- music.js
+++ music.js
@@ -5,6 +5,7 @@ export const musicLibrary = {
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
 "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=soundgallerybydmitrytaras-energetic-rock-333526.mp3",
         
     ],
     
@@ -60,7 +61,7 @@ export const musicLibrary = {
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
-        
+        "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=soundgallerybydmitrytaras-energetic-rock-333526.mp3",
     ],
     
     "Historical": [
@@ -87,7 +88,7 @@ export const musicLibrary = {
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
         "https://cdn.pixabay.com/download/audio/2025/10/14/audio_d7b510058d.mp3?filename=audioknap-rock-420049.mp3",
-        
+        "https://cdn.pixabay.com/download/audio/2025/04/28/audio_63ab78fd28.mp3?filename=soundgallerybydmitrytaras-energetic-rock-333526.mp3",
     ]
     
 };
