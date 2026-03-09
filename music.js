cc5908647e9a51bb89eb1ad095ac17f02a47dd5d
ojareridominion-prog <ojareridominion@gmail.com>
2026-03-09 01:52:23 +0100

    Update music.js

--- music.js
+++ music.js
@@ -10,24 +10,28 @@ export const musicLibrary = {
     "Places": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
-        
+"https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",        
               ],
     
     "Aesthetic": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
-       
+"https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
+        
     ],
     
     "Cars": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
-"https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",       
+"https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",  
+        "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
+        
     ],
     
     "Luxury": [
 
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
         
     ],
     
@@ -58,7 +62,7 @@ export const musicLibrary = {
     "Default": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
-        
+ "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",       
     ]
     
 };
