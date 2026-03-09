2e5061683fa1285c96dd4cc7c27761a4788c8753
ojareridominion-prog <ojareridominion@gmail.com>
2026-03-09 01:59:31 +0100

    Update music.js

--- music.js
+++ music.js
@@ -13,6 +13,7 @@ export const musicLibrary = {
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
 "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3", 
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
               ],
     
     "Aesthetic": [
@@ -20,6 +21,7 @@ export const musicLibrary = {
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
 "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
         
         
     ],
@@ -29,6 +31,7 @@ export const musicLibrary = {
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
 "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",  
         "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
         
     ],
     
@@ -37,6 +40,7 @@ export const musicLibrary = {
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
         
     ],
     
@@ -45,6 +49,7 @@ export const musicLibrary = {
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
  "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",  
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
               ],
     
     "Animals": [
@@ -57,12 +62,15 @@ export const musicLibrary = {
     "Historical": [
 
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
+        
         
     ],
     
       "Ai-generated": [
           
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
+          "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
           
           
     ],
@@ -72,6 +80,7 @@ export const musicLibrary = {
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
  "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",  
         "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/03/audio_6e76a1ff29.mp3?filename=audioknap-stylish-rock-446683.mp3",
         
     ]
     
