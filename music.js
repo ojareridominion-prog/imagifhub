2f5fae9f20d917d2cf8d5b03412d709aed52e077
ojareridominion-prog <ojareridominion@gmail.com>
2026-03-09 01:55:55 +0100

    Update music.js

--- music.js
+++ music.js
@@ -3,20 +3,24 @@ export const musicLibrary = {
     "Nature": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
-"https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",        
+"https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
         
     ],
     
     "Places": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
-"https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",        
+"https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3", 
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
               ],
     
     "Aesthetic": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
 "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        
         
     ],
     
@@ -32,18 +36,21 @@ export const musicLibrary = {
 
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
         "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
         
     ],
     
     "Art": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
- "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3*,       
+ "https://cdn.pixabay.com/download/audio/2025/12/05/audio_3580f915fd.mp3?filename=the_mountain-soft-piano-background-444129.mp3",  
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
               ],
     
     "Animals": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
         
     ],
     
@@ -57,12 +64,15 @@ export const musicLibrary = {
           
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
           
+          
     ],
     
     "Default": [
         
 "https://cdn.pixabay.com/download/audio/2025/07/22/audio_8fc5444c41.mp3?filename=blujle_music-cloud-kicker-is-awesome-378633.mp3",
- "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",       
+ "https://cdn.pixabay.com/download/audio/2025/10/13/audio_879d2ff61e.mp3?filename=ikoliks_aj-fun-pop-background-music-419356.mp3",  
+        "https://cdn.pixabay.com/download/audio/2025/12/05/audio_a8081cb8d3.mp3?filename=lightbeatsmusic-smilewave-pop-summer-443398.mp3",
+        
     ]
     
 };
