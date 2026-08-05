a1696c7179f3f2e491f4e037500f18e0c2c344d6
ojareridominion-prog <ojareridominion@gmail.com>
2026-08-05 01:05:48 +0100

    Add user property to state object

--- state.js
+++ state.js
@@ -14,5 +14,6 @@ export const state = {
     darkTextEnabled: false,
     isPremiumUser: false,
     paidPremiumActive: false,
-    nativeAds: []
+    nativeAds: [],
+    user: null  // <-- ADDED
 };
